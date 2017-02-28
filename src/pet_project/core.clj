fc86e1fe8c5495412f52fa06624f11391a5ef6a9
Raul Araya <nubeiro@gmail.com>
2017-03-01 00:04:16 +0100

    using maps with multiple sequences

--- src/pet_project/core.clj
+++ src/pet_project/core.clj
@@ -5,11 +5,26 @@
   [topic] 
   (str topic " for the Brave!"))
 
+(defn doMaps
+  []
+  (do 
+    (println (map titleize ["Hamsters" "Ragnarok"]))
+    (println (map titleize '("Empathy" "Decorating")))
+    (println (map titleize #{"Elbows" "Soap Carving"}))
+    (println (map #(titleize (first %))
+                  {:uncomfortable-thing "Winking"}))))
+
+(defn doMultipleMaps
+  []
+  (do
+    (map str 
+         ["a" "b" "c" "d"] 
+         ["A" "B" "C" "D"]
+         '("X" "Y" "Z"))
+)
+)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (map titleize ["Hamsters" "Ragnarok"]))
-  (println (map titleize '("Empathy" "Decorating")))
-  (println (map titleize #{"Elbows" "Soap Carving"}))
-  (println (map #(titleize (first %))
-                {:uncomfortable-thing "Winking"})))
+  (doMultipleMaps)
+)
